bb811bc5cf3c70476b42c233928d181437ad4da4
Joseph Schambach <joseph@vortechsolutions.com>
2025-04-04 10:09:09 -0400

    feat: Adding Connection for Database

--- my_app_backend/context/context_hsm.py
+++ my_app_backend/context/context_hsm.py
@@ -1,14 +1,37 @@
-from auth.authorizor import Auth
-from auth.session_manager import SessionManager
+from hsm_auth.authorizor import Auth
+from hsm_auth.session_manager import SessionManager
+from hsm_database.database_config import DataBaseConfig
+import json
 
 class ContextHSM:
     def __init__(self):
         self.authenticator = Auth()
         self.get_session_manager()
+        self.get_context()
+        self.get_secrets()
+        self.get_database()
 
     def get_context(self):
-        pass
+        with open('my_app_backend/context/config.json') as json_file:
+            data = json.load(json_file)
+            if data["context"] == "HSM": 
+                self.context = data
+        json_file.close()
+
+    def get_secrets(self):
+        secrets = self.context.get("secrets")
+        if secrets:
+            self.secrets = secrets
+        else:
+            raise ValueError("Secrets not found in context configuration")
 
     def get_session_manager(self):
         self.session_manager = SessionManager()
+
+    def get_database(self):
+        database = self.secrets.get("database")
+        if database:
+            self.database = DataBaseConfig(database)
+        else:
+            raise ValueError("Database not found in secrets configuration")
 
---
+++ my_app_backend/hsm_database/supabase_config.py
@@ -0,0 +1,11 @@
+from supabase import create_client, Client
+import pandas as pd
+
+class SupabaseConfig:
+    def __init__(self, auth_dict: dict):
+        self.url = auth_dict.get("supabase_url")
+        self.key = auth_dict.get("supabase_key")
+        self.service_role = auth_dict.get("supabase_service_role")
+        self.client: Client = create_client(self.url, self.service_role)
+        response = self.client.table("user_authentication_table").select("*").execute()
+        print(response.data)
